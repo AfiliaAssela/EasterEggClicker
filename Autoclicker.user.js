4b8e099c9281feda3725c375b58d0951a13725bb
Daniel Kampf <danielkampf1992@outlook.de>
2024-07-19 19:29:59 +0200

    Summer Update

--- Autoclicker.user.js
+++ Autoclicker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS Easter Egg Clicker
 // @namespace    www.leitstellenspiel.de
-// @version      1.3
+// @version      1.4
 // @description  Automatically click the Easter Egg link when an Easter Egg is detected
 // @author       Afilia
 // @match        https://www.leitstellenspiel.de/missions/*
@@ -65,6 +65,9 @@
             easterEggLink.click();
         } else if (imageSrc.includes('football')) {
             // For Football, automatically click the Easter Egg Link
+            easterEggLink.click();
+        } else if (imageSrc.includes('summer_item')) {
+            // For Flowers, automatically click the Easter Egg Link
             easterEggLink.click();
         }
     }
